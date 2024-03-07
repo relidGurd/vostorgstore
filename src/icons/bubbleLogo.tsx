7a8e35b2fc8f0d19c95d2a2a8021ed05e2ac57d9
Skillbox Student <titarevdeniz@yandex.ru>
2024-03-07 17:34:43 +0300

    rr

---
+++ src/icons/bubbleLogo.tsx
@@ -0,0 +1,29 @@
+const BubbleLogo = () => {
+  return (
+    <svg
+      version="1.1"
+      id="Слой_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlnsXlink="http://www.w3.org/1999/xlink"
+      x="0px"
+      y="0px"
+      viewBox="0 0 35.6 30"
+      style={{ background: `new 0 0 35.6 30;` }}
+      xmlSpace="preserve"
+    >
+      <g>
+        <g>
+          <image
+            style={{ overflow: "visible" }}
+            width="400px"
+            height="400px"
+            xlinkHref="E3D4EEC3.png"
+            transform="matrix(6.445312e-02 0 0 6.445312e-02 1.4677 0.9814)"
+          ></image>
+        </g>
+      </g>
+    </svg>
+  );
+};
+
+export default BubbleLogo;
--- src/pages/mainPage/mainPage.tsx
+++ src/pages/mainPage/mainPage.tsx
@@ -10,59 +10,109 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import "swiper/css/scrollbar";
 import { useAppSelector } from "@/lib/hooks";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import ButtonIcon from "@/icons/button";
+import BubbleLogo from "@/icons/bubbleLogo";
+import { InView, useInView } from "react-intersection-observer";
 
 const MainPage = ({ props }: any) => {
-  const count = useAppSelector((state) => state.counter.value);
+  const count = useAppSelector((state: any) => state.counter.value);
+  const { ref: sectionContacts, inView: inViewContacts, entry } = useInView();
+  const [parallaxPos, setParallaxPos] = useState(0);
   const [resizeVideoValue, setResizeVideoValue] = useState(35);
-  const [pos, setPos] = useState(0);
+  const [scrollNap, setScrollNap] = useState(0);
+  const {
+    ref: videoRef,
+    inView: inViewVideo,
+    entry: entryVideo,
+  } = useInView({
+    threshold: 1,
+    delay: 1000,
+  });
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
 
-  const resizeVideo = (e: any) => {
-    const windowPos = window.scrollY;
-    setPos(windowPos);
-    if (pos < window.scrollY) {
-      setResizeVideoValue(resizeVideoValue - 1);
-      if (resizeVideoValue <= 0) {
-        setResizeVideoValue(0);
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScroll = window.scrollY;
+
+      if (scrollNap < currentScroll) {
+        const newResizeVideoValue =
+          35 - (35 * currentScroll) / entryVideo?.target.clientHeight;
+        setResizeVideoValue(newResizeVideoValue < 0 ? 0 : newResizeVideoValue);
+      } else if (scrollNap > currentScroll && entryVideo?.isIntersecting) {
+        const newResizeVideoValue =
+          resizeVideoValue +
+          (35 * currentScroll) / entryVideo?.target.clientHeight;
+        setResizeVideoValue(
+          newResizeVideoValue > 35 ? 35 : newResizeVideoValue
+        );
       }
-    }
-  };
+    };
+
+    document.addEventListener("scroll", handleScroll);
+
+    return () => {
+      document.removeEventListener("scroll", handleScroll);
+    };
+  }, [scrollNap, entryVideo, resizeVideoValue]);
 
   useEffect(() => {
-    document.addEventListener("scroll", (e: any) => resizeVideo(e));
-    return () => document.removeEventListener("scroll", resizeVideo);
-  }, [resizeVideoValue]);
+    const handleScroll = (e: any) => {
+      const pos = window.scrollY;
+      if (inViewContacts) {
+        setParallaxPos((pos * 1) / 40);
+        // console.log("inv");
+      }
+    };
+    if (entry?.isIntersecting === true) {
+      document.addEventListener("scroll", handleScroll);
+    } else document.removeEventListener("scroll", handleScroll);
+    return () => document.removeEventListener("scroll", handleScroll);
+  }, [inViewContacts]);
 
   return (
-    <main>
+    <>
       <section className={`container ${styles.mainSliderContainer}`}>
-        <div className={styles.mainSlider}>
+        <div className={styles.verticalLine}></div>
+        <div ref={videoRef} className={styles.mainSlider}>
           <div className={styles.ggg}>
             <div className={styles.descriptionSection}>
               <div className={styles.descSideSec}>
                 <h1 className={styles.titleSlider}>
                   VOSTORG
-                  <span> _ ХУДОЖНИКИ</span>
+                  <span>
+                    <span className={styles.blinkSlach}>_</span> ХУДОЖНИКИ
+                  </span>
                 </h1>
-                <div className={styles.verticalLine}>d</div>
-                <div>123123134</div>
+                <div className={styles.slideMenuContainer}>
+                  <ul className={styles.slideMenu}>
+                    <li>О нас</li>
+                    <li>Магазин</li>
+                    <li>Художники</li>
+                    <li>Парнтнерам</li>
+                    <li>Контакты</li>
+                  </ul>
+                </div>
               </div>
               <div className={`${styles.descSideSec} ${styles.t}`}>
                 <div>
                   <LogoIcon />
                 </div>
-                <p>text</p>
+                <div className={styles.slideTextContainer}>
+                  <p className={styles.slideText}>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+                    Culpa, quidem! Suscipit, libero! Voluptatum
+                  </p>
+                </div>
               </div>
             </div>
             <div
               style={{
                 clipPath: `inset(${resizeVideoValue}% round 1rem)`,
-                transition: "clip-path 0.5s ease-out",
+                transition: "clip-path 0.5s ease",
               }}
               className={`${styles.videoSliderContainer}`}
             >
@@ -73,6 +123,7 @@ const MainPage = ({ props }: any) => {
           </div>
         </div>
       </section>
+
       <section style={{ paddingTop: "1rem" }} className={`container`}>
         <ul className={styles.cardList}>
           <li className={`${styles.card} ${styles.cardContainer}`}>
@@ -136,7 +187,280 @@ const MainPage = ({ props }: any) => {
           </li>
         </ul>
       </section>
-    </main>
+
+      <section className={`container ${styles.contactSection}`}>
+        <div className={styles.contactSectionItem}>
+          <div className={styles.aboutUsMoreSection}>
+            <h2>О нас</h2>
+            <div>
+              ©️{" "}
+              <span
+                style={{
+                  display: "inline-block",
+                  fontSize: "25px",
+                  fontWeight: "500",
+                }}
+              >
+                VOSTORG
+              </span>
+            </div>
+          </div>
+          <button className={styles.aboutUsButton}>Подробнее</button>
+        </div>
+      </section>
+
+      <section className={`container`}>
+        <ul className={styles.testContainer}>
+          <li className={styles.testContainerItem}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+              }}
+            >
+              <span>Что мы делаем</span>
+              <h3>Развиваем</h3>
+            </div>
+            <div
+              style={{
+                width: "100%",
+                height: "250px",
+                borderRadius: "1rem",
+                overflow: "hidden",
+              }}
+            >
+              <Image
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                width={1000}
+                height={1000}
+                alt={`el.title`}
+                src={`/1.webp`}
+              />
+            </div>
+          </li>
+          <li className={styles.testContainerItem}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+              }}
+            >
+              <span>Что мы делаем</span>
+              <h3>Вдохновляем</h3>
+            </div>
+            <div
+              style={{
+                width: "100%",
+                height: "250px",
+                borderRadius: "1rem",
+                overflow: "hidden",
+              }}
+            >
+              <Image
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                width={1000}
+                height={1000}
+                alt={`el.title`}
+                src={`/1.webp`}
+              />
+            </div>
+          </li>
+          <li className={styles.testContainerItem}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+              }}
+            >
+              <span>Что мы делаем</span>
+              <h3>Ебем</h3>
+            </div>
+            <div
+              style={{
+                width: "100%",
+                height: "250px",
+                borderRadius: "1rem",
+                overflow: "hidden",
+              }}
+            >
+              <Image
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                width={1000}
+                height={1000}
+                alt={`el.title`}
+                src={`/1.webp`}
+              />
+            </div>
+          </li>
+          <li className={styles.testContainerItem}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+              }}
+            >
+              <span>Что мы делаем</span>
+              <h3>Сосем</h3>
+            </div>
+            <div
+              style={{
+                width: "100%",
+                height: "250px",
+                borderRadius: "1rem",
+                overflow: "hidden",
+              }}
+            >
+              <Image
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+                width={1000}
+                height={1000}
+                alt={`el.title`}
+                src={`/1.webp`}
+              />
+            </div>
+          </li>
+        </ul>
+      </section>
+
+      <section className={`container`}>
+        <div
+          style={{ marginTop: "17rem", marginBottom: "5rem" }}
+          className={styles.testContainerItem}
+        >
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "space-between",
+            }}
+          >
+            <span>Что мы делаем</span>
+          </div>
+          <div
+            style={{
+              width: "100%",
+              height: "auto",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <p style={{ fontSize: "40px", fontWeight: 700, width: "50%" }}>
+              We help brands grow and tell their stories to the world.
+            </p>
+            <span style={{ fontSize: "40px", fontWeight: 700 }}>
+              <span className={styles.blinkSlach}>_</span>10
+            </span>
+          </div>
+        </div>
+        <ul className={styles.productsGrid}>
+          {props.data.map((el: any) => (
+            <li className={styles.productCard} key={el.id}>
+              <div className={styles.productCardImageContainer}>
+                <Image
+                  className={styles.productCardImage}
+                  width={1000}
+                  height={1000}
+                  alt={el.title}
+                  src={`/3.webp`}
+                />
+              </div>
+              <div className={styles.descriptionContainer}>
+                <span className={styles.descTitle}>{el.title}</span>
+                <span>Вася Пупкин</span>
+                <span>(2022)</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </section>
+
+      <section className={`container ${styles.contactSection}`}>
+        <div className={styles.contactSectionItem}>
+          <div className={styles.aboutUsMoreSection}>
+            <h2>О нас</h2>
+            <div>
+              ©️{" "}
+              <span
+                style={{
+                  display: "inline-block",
+                  fontSize: "25px",
+                  fontWeight: "500",
+                }}
+              >
+                VOSTORG
+              </span>
+            </div>
+          </div>
+          <button className={styles.aboutUsButton}>Подробнее</button>
+        </div>
+      </section>
+
+      <section
+        ref={sectionContacts}
+        style={{ paddingTop: "1rem" }}
+        className={`container`}
+      >
+        <div className={styles.parallaxSectionContainer}>
+          <div className={styles.parallaxBgSection}>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                height: "600px",
+              }}
+            >
+              <h2 style={{ color: "white", fontWeight: "500", width: "40%" }}>
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Lorem
+                ipsum dolor sit amet consectetur adipisicing elit. Numquam,
+                harum. Voluptas, eaque et.
+              </h2>
+              <div>
+                <span
+                  style={{
+                    color: "white",
+                    fontSize: "40px",
+                    fontWeight: "500",
+                  }}
+                >
+                  Остались вопросы?
+                </span>
+                <button
+                  className={styles.contactsButton}
+                  style={{
+                    marginTop: "20px",
+                    fontSize: "20px",
+                    fontWeight: "500",
+                    width: "100%",
+                    textAlign: "left",
+                    backgroundColor: "black",
+                    color: "white",
+                    padding: "1.5rem",
+                    borderRadius: "1rem",
+                  }}
+                >
+                  Контакты
+                </button>
+              </div>
+            </div>
+            <div
+              style={{
+                transform: `translateY(${parallaxPos}px)`,
+                transition: `transform 1s ease`,
+                zIndex: "-1",
+              }}
+              className={styles.parallaxBubbleLogo}
+            >
+              <BubbleLogo />
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
   );
 };
 
